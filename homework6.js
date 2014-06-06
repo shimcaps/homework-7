f0ed9ab049085165ff747c666d91ee280b097fb5
shimon caplan <shimcaps@gmail.com>
2014-06-05 21:00:30 -0400

    homework 6

---
+++ homework6.js
@@ -0,0 +1,70 @@
+
+
+var fruit = ["peaches", "banana", "orange", "mango"];
+ for(var i = 0; i < fruit.length; i++)
+ {
+ console.log ('my #' + (i + 1) + 'choice is ' +fruit [i]);
+ }
+ 
+ 
+ 
+ var aboutMe = {
+     hometown: "brooklyn, NY",
+     dob: "05-01-1987",
+     height: "5-10",
+ };
+ 
+ console.log(aboutMe);
+ 
+ aboutMe.gender = "male",
+ console.log(aboutMe);
+ 
+ aboutMe.job = {
+     name: "preferred services",
+     address: "1267 57th st brooklyn, NY 11219"
+     };
+ 
+ console.log(aboutMe);
+ 
+ aboutMe.name = "shimon caplan";
+ 
+ console.log(aboutMe);
+ 
+ aboutMe.job;
+ 
+ console.log(aboutMe.job);
+ console.log(aboutMe.job.address);
+ console.log(aboutMe.height);
+ 
+ aboutMe.phoneNumber = 9176529576;
+ 
+ console.log(aboutMe);
+ 
+ 
+ 
+ var myCats = [
+     {name: "dexter",
+     age: 5},
+     {name: "fluffy",
+     age: 10},
+     {name: "spott",
+     age: 8},
+     ];
+     
+     console.log(myCats);
+     
+     
+     myCats[0].weight = "16 lbs.";
+     
+     console.log(myCats);
+     
+     myCats[1].weight = "10 lbs.";
+     
+     console.log(myCats);
+     
+     myCats[2].weight = "13 lbs.";
+     
+     console.log(myCats);
+     
+     
+     
